9d95acdcbfdf3a084d0fb6f645d9a449ae912eb1
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-08-12 07:00:31 +0200

    feat: initialize empty spigot plugin

---
+++ src/main/java/pl/sxbrsky/stonedrop/StoneDrop.java
@@ -0,0 +1,17 @@
+package pl.sxbrsky.stonedrop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StoneDrop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
